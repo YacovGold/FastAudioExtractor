113baef9835350efa46ab2b9647cdb48fe883a76
yv <git@gmail.com>
2020-07-30 18:52:48 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace FastAudioExtractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_dir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_analyze = new System.Windows.Forms.Button();
+            this.btn_selectFolder = new System.Windows.Forms.Button();
+            this.lv = new System.Windows.Forms.ListView();
+            this.fileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.audioType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btn_extract = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_dir
+            // 
+            this.tb_dir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_dir.Location = new System.Drawing.Point(84, 12);
+            this.tb_dir.Name = "tb_dir";
+            this.tb_dir.Size = new System.Drawing.Size(735, 20);
+            this.tb_dir.TabIndex = 1;
+            this.tb_dir.Text = "D:\\tmp";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Video Folder";
+            // 
+            // btn_analyze
+            // 
+            this.btn_analyze.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_analyze.BackgroundImage = global::FastAudioExtractor.Properties.Resources.Crystal_Clear_app_kfind;
+            this.btn_analyze.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_analyze.Location = new System.Drawing.Point(861, 6);
+            this.btn_analyze.Name = "btn_analyze";
+            this.btn_analyze.Size = new System.Drawing.Size(30, 30);
+            this.btn_analyze.TabIndex = 4;
+            this.btn_analyze.UseVisualStyleBackColor = true;
+            this.btn_analyze.Click += new System.EventHandler(this.btn_analyze_Click);
+            // 
+            // btn_selectFolder
+            // 
+            this.btn_selectFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_selectFolder.BackgroundImage = global::FastAudioExtractor.Properties.Resources._128px_Human_folder_svg;
+            this.btn_selectFolder.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_selectFolder.Location = new System.Drawing.Point(825, 6);
+            this.btn_selectFolder.Name = "btn_selectFolder";
+            this.btn_selectFolder.Size = new System.Drawing.Size(30, 30);
+            this.btn_selectFolder.TabIndex = 2;
+            this.btn_selectFolder.UseVisualStyleBackColor = true;
+            this.btn_selectFolder.Click += new System.EventHandler(this.btn_selectFolder_Click);
+            // 
+            // lv
+            // 
+            this.lv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lv.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.fileName,
+            this.audioType});
+            this.lv.HideSelection = false;
+            this.lv.Location = new System.Drawing.Point(12, 38);
+            this.lv.Name = "lv";
+            this.lv.Size = new System.Drawing.Size(879, 499);
+            this.lv.TabIndex = 5;
+            this.lv.UseCompatibleStateImageBehavior = false;
+            this.lv.View = System.Windows.Forms.View.Details;
+            // 
+            // fileName
+            // 
+            this.fileName.Text = "File Name";
+            this.fileName.Width = 500;
+            // 
+            // audioType
+            // 
+            this.audioType.Text = "Audio Type";
+            this.audioType.Width = 200;
+            // 
+            // btn_extract
+            // 
+            this.btn_extract.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_extract.Enabled = false;
+            this.btn_extract.Location = new System.Drawing.Point(783, 543);
+            this.btn_extract.Name = "btn_extract";
+            this.btn_extract.Size = new System.Drawing.Size(108, 33);
+            this.btn_extract.TabIndex = 6;
+            this.btn_extract.Text = "Extract";
+            this.btn_extract.UseVisualStyleBackColor = true;
+            this.btn_extract.Click += new System.EventHandler(this.btn_extract_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 588);
+            this.Controls.Add(this.btn_extract);
+            this.Controls.Add(this.lv);
+            this.Controls.Add(this.btn_analyze);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_selectFolder);
+            this.Controls.Add(this.tb_dir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tb_dir;
+        private System.Windows.Forms.Button btn_selectFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_analyze;
+        private System.Windows.Forms.ListView lv;
+        private System.Windows.Forms.ColumnHeader fileName;
+        private System.Windows.Forms.ColumnHeader audioType;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btn_extract;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using FastAudioExtractor.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xabe.FFmpeg;
+using Xabe.FFmpeg.Downloader;
+
+namespace FastAudioExtractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tb_dir.Text = Settings.Default.LastFolder;
+        }
+
+        private void btn_selectFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tb_dir.Text = folderBrowserDialog1.SelectedPath;
+                btn_analyze.PerformClick();
+            }
+        }
+
+        private async void btn_analyze_Click(object sender, EventArgs e)
+        {
+            Settings.Default.LastFolder = tb_dir.Text;
+            Settings.Default.Save();
+
+            btn_extract.Enabled = false;
+
+            await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Full);
+
+            var extenssions = new List<string> { ".mov", "mp4" };
+
+            var files = Directory.EnumerateFiles(tb_dir.Text)
+                .Where(v => extenssions.Any(x => v.ToLower().EndsWith(x)));
+
+            foreach (var file in files)
+            {
+                var audioStream = (await FFmpeg.GetMediaInfo(file))
+                    .AudioStreams.FirstOrDefault();
+
+                var codec = "Unknown";
+                if (audioStream != null)
+                {
+                    codec = audioStream.Codec;
+                }
+
+                var lvi = new ListViewItem(new string[] { file, codec });
+                lv.Items.Add(lvi);
+            }
+
+            btn_extract.Enabled = true;
+        }
+
+        private void btn_extract_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lv.Items.Count; i++)
+            {
+                var lvi = lv.Items[i];
+
+                var inputFile = lvi.SubItems[0].Text;
+                var codec = lvi.SubItems[1].Text;
+                var outputFile = inputFile.Substring(0, inputFile.LastIndexOf('.') + 1) + codec;
+
+                Extract(inputFile, outputFile);
+            }
+
+            MessageBox.Show("Done.");
+        }
+
+        private void Extract(string inputFile, string outputFile)
+        {
+            var p = new Process();
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = true;
+
+            var args = $"-i \"{inputFile}\" -vn -acodec copy \"{outputFile}\"";
+
+            Process.Start($"ffmpeg.exe", args).WaitForExit();
+        }
+    }
+}
